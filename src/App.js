96906545e232dd23f5a0dfeb8affe2fe4c44d201
nickj <nickjiunchetti@gmail.com>
2021-03-07 19:42:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme, GlobalStyles } from './themes'
+
+const StyledApp = styled.div`
+	color: ${(props) => props.theme.fontColor};
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useState(true)
+
+	const themeToggler = () => {
+		setTheme(!theme)
+	}
+
+	return (
+		<ThemeProvider theme={theme ? darkTheme : lightTheme}>
+			<GlobalStyles />
+			<StyledApp>
+				Teste
+				<button onClick={themeToggler}>Change Theme</button>
+			</StyledApp>
+		</ThemeProvider>
+	)
 }
 
 export default App;
